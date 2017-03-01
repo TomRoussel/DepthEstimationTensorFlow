eba82b4dce4a5c1bec965b4dffa5fa5d42f2c4da
Tom Roussel <tom.roussel@esat.kuleuven.be>
2017-03-01 18:07:14 +0100

    Implemented NYU preprocessing, converting the endless batch of mat files to one nice hdf file.

---
+++ src/preprocess_scripts/NYU_preprocess.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# @Author: Tom Roussel
+# @Date:   2017-02-07 17:22:39
+# @Last Modified by:   Tom Roussel
+# @Last Modified time: 2017-03-01 18:03:32
+
+import numpy as np
+import h5py
+import sys; sys.path.append("../util")
+import util
+from glob import glob
+
+outPath = "/esat/citrine/tmp/troussel/IROS/kinect/NYU_data/NYU.hdf5"
+labelFolder = "/esat/emerald/pchakrav/StijnData/NYUv2/processed/labels_filled_proc/"
+
+def all_labels(labelFolder):
+	allFiles = glob(labelFolder + "/labels_*.mat")
+	filteredFiles = [x for x in allFiles if not "_esat" in x]
+
+	allLabels = []
+	for f in filteredFiles:
+		mat = util.loadmat(f)
+		for label in mat["labels_name"]:
+			allLabels.append(label + ",%s" % f)
+
+	return allLabels
+
+def main():
+	print("Fetching labels")
+	labels = all_labels(labelFolder)
+
+	f = h5py.File(outPath, 'w')
+	depth = f.create_group("depth").create_dataset("depth_data",(55, 74,len(labels)), compression="gzip", dtype=np.float64)
+	dt = h5py.special_dtype(vlen=bytes)
+	depth_label = f["depth"].create_dataset("depth_labels", [len(labels)], dtype=dt)
+	
+	allFiles = glob(labelFolder + "/labels_*.mat")
+	filteredFiles = [x for x in allFiles if not "_esat" in x]
+
+	startI = 0
+
+	fileNo = 1
+
+	# Loop over all data
+	for x in filteredFiles:
+		print("Processing file %d/%d" % (fileNo, len(filteredFiles)))
+		fdata = util.loadmat(x)
+		endI = startI + fdata["labels_name"].shape[0]
+		depth[:,:,startI:endI] = fdata["labels_processed"]
+		depth_label[startI:endI] = fdata["labels_name"]
+
+		# Update startI
+		startI = endI
+
+		fileNo += 1
+
+if __name__ == '__main__':
+	main()
