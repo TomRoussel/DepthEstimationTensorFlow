a7d3c73954d9af9d041cc3707334d85bab7d202e
Tom Roussel <tom.roussel@esat.kuleuven.be>
2017-03-02 11:00:59 +0100

    Added extra info in hdf file for NYU data

--- src/preprocess_scripts/NYU_preprocess.py
+++ src/preprocess_scripts/NYU_preprocess.py
@@ -2,13 +2,14 @@
 # @Author: Tom Roussel
 # @Date:   2017-02-07 17:22:39
 # @Last Modified by:   Tom Roussel
-# @Last Modified time: 2017-03-01 18:03:32
+# @Last Modified time: 2017-03-02 10:16:09
 
 import numpy as np
 import h5py
 import sys; sys.path.append("../util")
 import util
 from glob import glob
+import os.path
 
 outPath = "/esat/citrine/tmp/troussel/IROS/kinect/NYU_data/NYU.hdf5"
 labelFolder = "/esat/emerald/pchakrav/StijnData/NYUv2/processed/labels_filled_proc/"
@@ -33,6 +34,7 @@ def main():
 	depth = f.create_group("depth").create_dataset("depth_data",(55, 74,len(labels)), compression="gzip", dtype=np.float64)
 	dt = h5py.special_dtype(vlen=bytes)
 	depth_label = f["depth"].create_dataset("depth_labels", [len(labels)], dtype=dt)
+	depth_mat_source = f["depth"].create_dataset("depth_folder_id", [len(labels)], dtype=np.uint16)
 	
 	allFiles = glob(labelFolder + "/labels_*.mat")
 	filteredFiles = [x for x in allFiles if not "_esat" in x]
@@ -44,10 +46,12 @@ def main():
 	# Loop over all data
 	for x in filteredFiles:
 		print("Processing file %d/%d" % (fileNo, len(filteredFiles)))
+		matFileNo = int(os.path.split(x)[-1].split("_")[1])
 		fdata = util.loadmat(x)
 		endI = startI + fdata["labels_name"].shape[0]
 		depth[:,:,startI:endI] = fdata["labels_processed"]
 		depth_label[startI:endI] = fdata["labels_name"]
+		depth_mat_source[startI:endI] = matFileNo
 
 		# Update startI
 		startI = endI
