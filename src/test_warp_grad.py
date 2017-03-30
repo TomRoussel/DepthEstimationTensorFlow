Fixed some bugs, finished gradient implementation (further testing required)

src/test_warp_grad.py
@@ -2,7 +2,7 @@
 # @Author: Tom Roussel
 # @Date:   2017-03-16 14:00:33
 # @Last Modified by:   Tom Roussel
-# @Last Modified time: 2017-03-30 17:12:02
+# @Last Modified time: 2017-03-30 17:17:08
 import tensorflow as tf
 import numpy as np
 import util.SEDWarp as SEDWarp
@@ -11,38 +11,27 @@ import matplotlib.pyplot as plt
 import util.util as util
 from scipy.misc import imresize
 
-from time import time
 
 fnXML = "/users/visics/troussel/tmp/test_SIM3.xml"
 bpath = "/esat/citrine/troussel/IROS/kinect/Kinect2/tmp_data/office4/RGB"
 bpathDepth = "/esat/citrine/troussel/IROS/kinect/Kinect2/tmp_data/office4/depth"
 
-def warpNormal(frame, depth, poseM):
-	warped = SEDWarp2.warp_image_opt(frame,depth,poseM)
-	return warped
 
 def warpTF(frame, depth, poseM):
 	# Make placeholder graph
 	inputGraph = tf.placeholder(tf.float32, shape = [2, 480, 640], name="RGB_in")
 	depthGraph = tf.placeholder(tf.float32, shape = [2, 480, 640], name="depth_in")
 	poseMGraph = tf.placeholder(tf.float32, shape = [2,4,4], name="poseM")
-	# Properly reshape frame and depth
-	# greyFlat = tf.reshape(inputGraph, (2,480*640))
-	# depthFlat = tf.reshape(depthGraph, (2,480*640))
 	# Build warping graph
 	warpGraph = SEDWarp.warp_graph(depthGraph, inputGraph, poseMGraph)
-	print(tf.gradients(warpGraph, depthGraph))
+	gradGraph = tf.gradients(warpGraph, depthGraph)
 
 	# Start tensorflow session
 	with tf.Session(config=tf.ConfigProto(device_count = {'GPU': 0})) as sess:
 		# Run graph
-		t1 = time()
-		result = sess.run(warpGraph, feed_dict={inputGraph:frame, depthGraph: depth, poseMGraph: poseM})
-		t2 = time()
+		result, grad = sess.run((warpGraph, gradGraph), feed_dict={inputGraph:frame, depthGraph: depth, poseMGraph: poseM})
 
-	print("TF implementation took %f seconds" % (t2 - t1))
-
-	return result
+	return result, grad
 
 def main():
 	# Load single image-pair
@@ -55,38 +44,14 @@ def main():
 	depth1 = imresize(depth1, (480,640))
 	frame2 = imresize(frame2, (480,640))
 	depth2 = imresize(depth2, (480,640))
-	# Warp image using normal implementation
-	t1 = time()
-	warpedNormal1 = warpNormal(frame1, depth1, poseM1)
-	warpedNormal2 = warpNormal(frame2, depth2, poseM2)
-	t2 = time()
-	print("Numpy implementation took %f seconds" % (t2 - t1))
 	# Warp image using tensorflow implementation
 	frames = np.stack([frame1, frame2], axis=0)
 	depths = np.stack([depth1, depth2], axis=0)
 	poses = np.stack([poseM1, poseM2], axis=0)
-	warpedTF = warpTF(frames, depths, poses)
+	warpedTF, grads = warpTF(frames, depths, poses)
 	# print warpedTF
 	# Show results for both
-	plt.subplot(231)
-	plt.imshow(warpedNormal1, cmap = 'gray')
-	plt.title("Normal warping")
-	plt.subplot(232)
-	plt.imshow(warpedTF[0], cmap = 'gray')
-	plt.title("TensorFlow warping")
-	plt.subplot(233)
-	plt.imshow(warpedTF[0] - warpedNormal1, cmap = 'gray')
-	plt.title("Difference")
-	plt.subplot(234)
-	plt.imshow(warpedNormal2, cmap = 'gray')
-	plt.title("Normal warping")
-	plt.subplot(235)
-	plt.imshow(warpedTF[1], cmap = 'gray')
-	plt.title("TensorFlow warping")
-	plt.subplot(236)
-	plt.imshow(warpedTF[1] - warpedNormal2, cmap = 'gray')
-	plt.title("Difference")
-	plt.show()
+	import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
 	main()

src/util/SEDWarp.py
@@ -2,23 +2,22 @@
 # @Author: Tom Roussel
 # @Date:   2017-03-16 13:59:42
 # @Last Modified by:   Tom Roussel
-# @Last Modified time: 2017-03-30 14:53:23
+# @Last Modified time: 2017-03-30 18:19:37
 
 import tensorflow as tf
 import numpy as np
 
 # TODO: Make this a relative path
 # TODO: OR make this using py_func
-zeroOut2 = tf.load_op_library('/users/visics/troussel/Tensor_Workspace/Python_Code/depth_estim/src/test/zero_out2.so').zero_out2
+# zeroOut2 = tf.load_op_library('/users/visics/troussel/Tensor_Workspace/Python_Code/depth_estim/src/test/zero_out2.so').zero_out2
+zeroOut3 = tf.load_op_library('/users/visics/troussel/Tensor_Workspace/Python_Code/depth_estim/src/test/zero_out2.so').zero_out3
 
 def image_gradient(image):
 	return np.gradient(image, axis = (1,2))
 
-# TODO: Take oobpixels into account
-@tf.RegisterGradient("ZeroOut2")
+@tf.RegisterGradient("ZeroOut3")
 def test_gradient_register(operation, grad):
-	import pdb; pdb.set_trace()
-	image, omega = operation.inputs
+	image, omega, oobPixels = operation.inputs
 	batchSize = int(image.shape[0])
 	shape = [int(x) for x in image.shape][1:]
 	pixelAmount = shape[0] * shape[1]
@@ -30,9 +29,12 @@ def test_gradient_register(operation, grad):
 
 	omegaFlat = tf.reshape(omegaFlat, (batchSize,-1))
 	omegaFlat = tf.round(omegaFlat)
+	omegaFlat = omegaFlat * tf.cast(tf.logical_not(oobPixels), dtype = tf.float32)
 	batchNo = tf.tile(tf.reshape(tf.linspace(0.0,float(batchSize-1), batchSize), (-1,1)), (1,pixelAmount))
+	
 	indexes = tf.stack([batchNo,omegaFlat], axis = 2)
 	indexes = tf.cast(indexes, tf.int32)
+	# indexes = tf.Print(indexes, [indexes], message="indexes", summarize = 1e6)
 
 	# Get gradient image
 	gradientImage = tf.stack(tf.py_func(image_gradient, [image], [tf.float32,tf.float32]), axis=1) # [batches, dimensions, H, W]
@@ -44,7 +46,7 @@ def test_gradient_register(operation, grad):
 
 	outGrad = tf.stack([fx*gradFlat, fy*gradFlat], axis = 1)
 
-	return None, outGrad
+	return None, outGrad, None
 
 def normalize_image_points(coords, image_shape):
 	"""
@@ -147,6 +149,6 @@ def warp_graph(depth, inGray, poseM):
 	# omega_dn = tf.Print(omega_dn, [ppNormTensor, positionV, projectedPoints, omega, oobPixels], summarize = 1e6)
 	warped = warp_using_coords(inGray, omega_dn, oobPixels)
 	# Override gradient
-	B = zeroOut2(warped, omega_dn)
+	B = zeroOut3(warped, omega_dn, oobPixels)
 	warped = B + tf.stop_gradient(warped - B)
 	return warped